c221f11b5f561c070e4e78ad1d7729c06ab9bf75
Aoc4456 <sizu44567789@gmail.com>
2021-07-10 19:22:40 +0900

    Fragmentからのxml呼び出しを削除する

--- app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListFragment.kt
@@ -6,27 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.aoc4456.radarchart.R
+import com.aoc4456.radarchart.databinding.GroupListFragmentBinding
 
 class GroupListFragment : Fragment() {
 
     private lateinit var viewModel: GroupListViewModel
+    private lateinit var binding: GroupListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.group_list_fragment, container, false)
+    ): View {
+        binding = GroupListFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(GroupListViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
-    companion object {
-        fun newInstance() = GroupListFragment()
     }
 }
