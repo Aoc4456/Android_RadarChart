cd3578b537714b199c89797192392da74e5b2614
Aoc4456 <sizu44567789@gmail.com>
2021-07-16 16:51:34 +0900

    バツボタンで画面を閉じる

--- app/src/main/java/com/aoc4456/radarchart/screen/groupcreate/GroupCreateFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/groupcreate/GroupCreateFragment.kt
@@ -6,26 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.aoc4456.radarchart.R
+import androidx.navigation.fragment.findNavController
+import com.aoc4456.radarchart.databinding.GroupCreateFragmentBinding
 
 class GroupCreateFragment : Fragment() {
 
+    private lateinit var binding: GroupCreateFragmentBinding
     private lateinit var viewModel: GroupCreateViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.group_create_fragment, container, false)
+    ): View {
+        binding = GroupCreateFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(GroupCreateViewModel::class.java)
-    }
 
-    companion object {
-        fun newInstance() = GroupCreateFragment()
+        binding.toolbarCloseButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
