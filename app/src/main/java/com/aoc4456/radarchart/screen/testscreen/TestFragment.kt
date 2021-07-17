6f52017e0dc46d719f1bbb75ad9cc0252996616a
Aoc4456 <sizu44567789@gmail.com>
2021-07-17 11:26:54 +0900

    テスト用のページにチャートを表示する

--- app/src/main/java/com/aoc4456/radarchart/screen/testscreen/TestFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/testscreen/TestFragment.kt
@@ -1,5 +1,6 @@
 package com.aoc4456.radarchart.screen.testscreen
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aoc4456.radarchart.databinding.FragmentTestBinding
+import com.github.mikephil.charting.data.RadarData
+import com.github.mikephil.charting.data.RadarDataSet
+import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 
 class TestFragment : Fragment() {
 
@@ -27,5 +32,52 @@ class TestFragment : Fragment() {
         binding.toolbarCloseButton.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        binding.radarChart.let {
+            it.isRotationEnabled = false
+            it.legend.isEnabled = false
+            it.isHighlightPerTapEnabled = false
+
+            it.xAxis.valueFormatter = TestXAxisFormatter()
+
+            it.yAxis.setLabelCount(6, true)
+            it.yAxis.setDrawLabels(false)
+            it.yAxis.axisMinimum = 0F
+            it.yAxis.axisMaximum = 10f
+        }
+
+        val radarChartEntry = mutableListOf<RadarEntry>(
+            RadarEntry(3f),
+            RadarEntry(2f),
+            RadarEntry(6f),
+            RadarEntry(4f),
+            RadarEntry(5f),
+        )
+
+        // ラベルを変えるテスト
+        (binding.radarChart.xAxis.valueFormatter as IndexAxisValueFormatter).values =
+            listOf("項目1", "項目2", "項目3", "項目4", "項目5").toTypedArray()
+
+        val radarChartDataSet = RadarDataSet(radarChartEntry, "ラベルです")
+        radarChartDataSet.color = Color.BLUE
+        radarChartDataSet.fillColor = Color.BLUE
+        radarChartDataSet.setDrawFilled(true)
+        radarChartDataSet.valueFormatter = IndexAxisValueFormatter()
+
+        val radarData = RadarData(radarChartDataSet)
+
+        binding.radarChart.data = radarData
+        binding.radarChart.data.notifyDataChanged()
+        binding.radarChart.notifyDataSetChanged()
     }
 }
+
+class TestXAxisFormatter : IndexAxisValueFormatter(
+    listOf(
+        "aa",
+        "bb",
+        "cc",
+        "dd",
+        "ee"
+    )
+)
