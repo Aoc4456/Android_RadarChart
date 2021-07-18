47676e02cf2cb80c1a4a7db565007a36a505182d
Aoc4456 <sizu44567789@gmail.com>
2021-07-18 13:18:55 +0900

    Viewの設置と削除

--- app/src/main/java/com/aoc4456/radarchart/component/MultiEditText.kt
+++ app/src/main/java/com/aoc4456/radarchart/component/MultiEditText.kt
@@ -8,35 +8,69 @@ import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import com.aoc4456.radarchart.R
 import com.aoc4456.radarchart.util.setMargin
-import timber.log.Timber
 
-class MultiEditText(context: Context, attrs: AttributeSet) :
+/**
+ * 複数個の項目名を入力するためのView
+ * EditTextたちをホストする
+ *
+ * 使い方
+ * 1. xmlで app:textList="@{viewmodel.textList}"
+ * 2. textListが更新されると、Viewの設置とテキストのセットが行われる
+ */
+class MultiEditText(context: Context, private val attrs: AttributeSet) :
     LinearLayout(context, attrs),
     MultiEditTextInput {
 
-    private val scale = resources.displayMetrics.density
-
-    private var numberOfItems = 5 // TODO 初期値をどうにかする
+    private val editTextList = mutableListOf<EditText>()
 
     init {
         orientation = VERTICAL
-        repeat(numberOfItems) {
-            val editText = createEditText(attrs)
-            addView(editText)
-            val topMargin = (8 * scale + 0.5f).toInt()
-            editText.setMargin(top = topMargin)
-        }
-    }
-
-    private fun createEditText(attrs: AttributeSet): EditText {
-        val editText = BorderEditText(context, attrs)
-        editText.inputType = InputType.TYPE_CLASS_TEXT
-        editText.height = resources.getDimensionPixelSize(R.dimen.edit_text_height)
-        return editText
     }
 
     override fun changeNumberOfItems(textList: List<String>) {
-        Timber.d("テキストチェンジ : $textList")
+        val diff = kotlin.math.abs(editTextList.size - textList.size)
+        if (diff == 0) return
+
+        if (editTextList.size < textList.size) {
+            appendItems(diff, textList)
+        } else {
+            removeItems(diff)
+        }
+    }
+
+    private fun appendItems(appendNum: Int, textList: List<String>) {
+        val currentNum = editTextList.size
+        for (i in currentNum until (currentNum + appendNum)) {
+            val editText = createEditText(textList[i])
+            placeView(editText)
+        }
+    }
+
+    private fun removeItems(removeNum: Int) {
+        for (i in 0 until removeNum) {
+            val removeEditText = editTextList.last()
+            editTextList.remove(removeEditText)
+            removeView(removeEditText)
+        }
+    }
+
+    private fun createEditText(text: String?): EditText {
+        val editText = BorderEditText(context, attrs)
+        editText.inputType = InputType.TYPE_CLASS_TEXT
+        editText.height = resources.getDimensionPixelSize(R.dimen.edit_text_height)
+        text?.let {
+            editText.setText(it)
+        }
+        return editText
+    }
+
+    private fun placeView(view: EditText) {
+        editTextList.add(view)
+        addView(view)
+
+        val scale = resources.displayMetrics.density
+        val topMargin = (8 * scale + 0.5f).toInt()
+        view.setMargin(top = topMargin)
     }
 }
 
