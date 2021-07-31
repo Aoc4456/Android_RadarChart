c5979fe969b20869ab24b05a16c314c354af891f
Aoc4456 <sizu44567789@gmail.com>
2021-07-31 19:17:50 +0900

    リストを長押ししたときにContextMenuを表示

--- app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListViewModel.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import com.aoc4456.radarchart.datasource.RadarChartRepository
 import com.aoc4456.radarchart.datasource.database.GroupWithLabelAndCharts
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +15,12 @@ class GroupListViewModel @Inject constructor(
 
     private val _groupList = repository.observeChartGroupList()
     val groupList: LiveData<List<GroupWithLabelAndCharts>> = _groupList
+
+    fun onClickListItem(position: Int) {
+        Timber.d("クリックポジション = $position")
+    }
+
+    fun onSelectedContextMenu(position: Int, itemId: Int) {
+        Timber.d("長押しポジション = $position  itemId = $itemId")
+    }
 }
