26ddc097d4c704a396887584a62aca4147a0ad07
Aoc4456 <sizu44567789@gmail.com>
2021-08-01 18:39:19 +0900

    遷移イベントが無限に通知されるのを防ぐ

--- app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListViewModel.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/grouplist/GroupListViewModel.kt
@@ -1,11 +1,11 @@
 package com.aoc4456.radarchart.screen.grouplist
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.aoc4456.radarchart.R
 import com.aoc4456.radarchart.datasource.RadarChartRepository
 import com.aoc4456.radarchart.datasource.database.GroupWithLabelAndCharts
+import com.aoc4456.radarchart.util.PublishLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
@@ -18,8 +18,8 @@ class GroupListViewModel @Inject constructor(
     private val _groupList = repository.observeChartGroupList()
     val groupList: LiveData<List<GroupWithLabelAndCharts>> = _groupList
 
-    private val _navigateToGroupEdit = MutableLiveData<GroupWithLabelAndCharts>()
-    val navigateToGroupEdit: LiveData<GroupWithLabelAndCharts> = _navigateToGroupEdit
+    private val _navigateToGroupEdit = PublishLiveData<GroupWithLabelAndCharts?>()
+    val navigateToGroupEdit: PublishLiveData<GroupWithLabelAndCharts?> = _navigateToGroupEdit
 
     fun onClickListItem(position: Int) {
         Timber.d("クリックポジション = $position")
@@ -29,6 +29,7 @@ class GroupListViewModel @Inject constructor(
         Timber.d("長押しポジション = $position  itemId = $itemId")
         when (itemId) {
             R.id.group_edit -> {
+                _navigateToGroupEdit.value = groupList.value!![position]
             }
             R.id.sorting_items -> {
             }
