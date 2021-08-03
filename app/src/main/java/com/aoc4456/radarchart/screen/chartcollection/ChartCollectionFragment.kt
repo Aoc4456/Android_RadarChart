840be875763b58297391655b24630104c0abbe5b
Aoc4456 <sizu44567789@gmail.com>
2021-08-03 21:56:24 +0900

    画面遷移 & 値渡し

--- app/src/main/java/com/aoc4456/radarchart/screen/chartcollection/ChartCollectionFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/chartcollection/ChartCollectionFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.aoc4456.radarchart.databinding.ChartCollectionFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +15,8 @@ class ChartCollectionFragment : Fragment() {
 
     private val viewModel by viewModels<ChartCollectionViewModel>()
     private lateinit var binding: ChartCollectionFragmentBinding
+
+    private val navArgs: ChartCollectionFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,5 +27,10 @@ class ChartCollectionFragment : Fragment() {
         binding.lifecycleOwner = this.viewLifecycleOwner
         binding.viewmodel = this.viewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navArgsText.text = navArgs.groupWithLabelAndCharts!!.group.title
     }
 }
