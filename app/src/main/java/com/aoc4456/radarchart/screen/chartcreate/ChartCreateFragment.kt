66feb2c8862f6d1f8ce5db384fdff3b4b556dd63
Aoc4456 <sizu44567789@gmail.com>
2021-08-04 17:45:50 +0900

    チャート作成画面への遷移を実装

--- app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.aoc4456.radarchart.databinding.ChartCreateFragmentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChartCreateFragment : Fragment() {
 
     private lateinit var binding: ChartCreateFragmentBinding
