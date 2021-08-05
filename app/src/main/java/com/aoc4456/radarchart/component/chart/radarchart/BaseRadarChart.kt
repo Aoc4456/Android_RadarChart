259a0e7439d1efe155930178ed4e0cf3cd041e4f
Aoc4456 <sizu44567789@gmail.com>
2021-08-05 20:29:51 +0900

    ChartTypeによって属性を変更する

--- app/src/main/java/com/aoc4456/radarchart/component/chart/radarchart/BaseRadarChart.kt
+++ app/src/main/java/com/aoc4456/radarchart/component/chart/radarchart/BaseRadarChart.kt
@@ -3,6 +3,7 @@ package com.aoc4456.radarchart.component.chart.radarchart
 import android.content.Context
 import android.util.AttributeSet
 import androidx.databinding.BindingAdapter
+import com.aoc4456.radarchart.R
 import com.github.mikephil.charting.charts.RadarChart
 import com.github.mikephil.charting.data.RadarData
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
@@ -10,12 +11,15 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 /**
  * RadarChartのBaseクラス
  *
- * 分割数 : 6
- * 最大値 : 100
+ * 使い方
+ * 1. xml で app:chartTypeを設定
+ * 2. 動的にチャートの状態を変更させる場合は、各BindingAdapterを設定する
  */
 open class BaseRadarChart(context: Context, attrs: AttributeSet) :
     RadarChart(context, attrs),
     RadarChartInput {
+
+    private var chartType = ChartType.DEFAULT
 
     init {
         isRotationEnabled = false
@@ -29,10 +33,49 @@ open class BaseRadarChart(context: Context, attrs: AttributeSet) :
         yAxis.setLabelCount(6, true)
         yAxis.axisMinimum = 0f
         yAxis.axisMaximum = 100f
+
+        // get custom property
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.BaseRadarChart,
+            0, 0
+        ).apply {
+            try {
+                val chartTypeInt =
+                    getInteger(R.styleable.BaseRadarChart_chartType, ChartType.DEFAULT.value)
+                chartType = ChartType.fromInt(chartTypeInt)
+            } finally {
+                recycle()
+            }
+        }
+
+        applyChartType(chartType)
+    }
+
+    fun applyChartType(type: ChartType) {
+        this.chartType = type
+        when (type) {
+            ChartType.GROUP_LIST -> {
+                yAxis.setLabelCount(4, true)
+                yAxis.axisMaximum = 90f
+                fixedLongestLabel = "12345"
+            }
+        }
     }
 
     override fun setChartItemLabel(labels: List<String>) {
         (xAxis.valueFormatter as IndexAxisValueFormatter).values = labels.toTypedArray()
+    }
+}
+
+enum class ChartType(val value: Int) {
+    DEFAULT(-1),
+    GROUP_LIST(0),
+    GROUP_CREATE(1),
+    CHART_CREATE(2);
+
+    companion object {
+        fun fromInt(value: Int) = values().first { it.value == value }
     }
 }
 
