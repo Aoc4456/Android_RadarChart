43a939762f76fcaaf4563067d99683c3eef3c6b5
Aoc4456 <sizu44567789@gmail.com>
2021-08-06 17:32:57 +0900

    チャート作成画面：初期値のバインディング(途中)

--- app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateViewModel.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateViewModel.kt
@@ -1,11 +1,50 @@
 package com.aoc4456.radarchart.screen.chartcreate
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import com.aoc4456.radarchart.datasource.RadarChartRepository
+import com.aoc4456.radarchart.datasource.database.GroupWithLabelAndCharts
+import com.aoc4456.radarchart.datasource.database.MyChart
+import com.github.mikephil.charting.data.RadarData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ChartCreateViewModel @Inject constructor(
     private val repository: RadarChartRepository
-) : ViewModel()
+) : ViewModel() {
+
+    private val groupData = MutableLiveData<GroupWithLabelAndCharts>()
+
+    private val _chartArgs = MutableLiveData<MyChart>()
+    val chartArgs: LiveData<MyChart> = _chartArgs
+
+    val chartLabels: LiveData<List<String>> = groupData.map { group ->
+        group.labelList.map { it.text }
+    }
+
+    val chartMaximum: LiveData<Int> = groupData.map { it.group.maximumValue }
+
+    private val _chartColor = MutableLiveData<Int>()
+    val chartColor: LiveData<Int> = _chartColor
+
+    private val _chartData = MutableLiveData<RadarData>()
+    val chartData: LiveData<RadarData> = _chartData
+
+    private val _chartUpdate = MutableLiveData<Boolean>()
+    val chartUpdate: LiveData<Boolean> = _chartUpdate
+
+    fun onViewCreated(args: ChartCreateFragmentArgs) {
+        groupData.value = args.groupWithLabelAndCharts
+        if (args.chart == null) {
+            _chartColor.value = groupData.value!!.group.color
+            return
+        }
+        _chartArgs.value = args.chart
+        chartArgs.value?.let {
+            _chartColor.value = it.color
+        }
+    }
+}
