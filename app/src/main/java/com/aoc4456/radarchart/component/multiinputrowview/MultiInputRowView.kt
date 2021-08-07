2cfffb6d6e2f9eca41bd5598353ed22db347bf3b
Aoc4456 <sizu44567789@gmail.com>
2021-08-07 16:52:28 +0900

    初期値を受け取るインターフェースを作成

--- app/src/main/java/com/aoc4456/radarchart/component/multiinputrowview/MultiInputRowView.kt
+++ app/src/main/java/com/aoc4456/radarchart/component/multiinputrowview/MultiInputRowView.kt
@@ -8,17 +8,36 @@ import com.aoc4456.radarchart.util.setMargin
 class MultiInputRowView(
     context: Context,
     private val attrs: AttributeSet
-) : LinearLayout(context, attrs) {
+) : LinearLayout(context, attrs), MultiInputViewInput {
 
-    val scale = resources.displayMetrics.density
+    private val scale = resources.displayMetrics.density
+    private val rowViewList = mutableListOf<InputRowView>()
+
+    private var maximum: Int = 100
+    lateinit var onChangeValueCallback: (index: Int, newValue: Int) -> Unit
+
+    private val defaultValue get() = (maximum * 0.6).toInt()
 
     init {
         orientation = VERTICAL
-        repeat(5) {
-            val rowView = InputRowView(context, attrs)
-            rowView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, (36 * scale + 0.5f).toInt())
-            placeView(rowView)
-        }
+    }
+
+    private fun createRowView(label: String, value: Int?): InputRowView {
+        val rowView = InputRowView(context, attrs)
+
+        val rowHeight = (36 * scale + 0.5f).toInt()
+        rowView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, rowHeight)
+
+        rowView.setup(
+            labelName = label,
+            initialValue = value ?: defaultValue,
+            maximum = this.maximum,
+            onChangeValueCallback = { view: InputRowView, newValue: Int ->
+                val index = rowViewList.indexOf(view)
+                this.onChangeValueCallback(index, newValue)
+            }
+        )
+        return rowView
     }
 
     private fun placeView(view: InputRowView) {
@@ -26,4 +45,28 @@ class MultiInputRowView(
         val topMargin = (5 * scale + 0.5f).toInt()
         view.setMargin(top = topMargin)
     }
+
+    override fun setup(
+        labels: List<String>,
+        values: List<Int>?,
+        maximum: Int,
+        onChangeValueCallback: (index: Int, newValue: Int) -> Unit
+    ) {
+        this.maximum = maximum
+        this.onChangeValueCallback = onChangeValueCallback
+        for (i in labels.indices) {
+            val rowView = createRowView(labels[i], values?.getOrNull(i))
+            placeView(rowView)
+            rowViewList.add(rowView)
+        }
+    }
+}
+
+interface MultiInputViewInput {
+    fun setup(
+        labels: List<String>,
+        values: List<Int>?,
+        maximum: Int,
+        onChangeValueCallback: (index: Int, newValue: Int) -> Unit
+    )
 }
