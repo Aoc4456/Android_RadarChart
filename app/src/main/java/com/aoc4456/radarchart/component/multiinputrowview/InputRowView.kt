ea7ba7e12c17048905d39d0e601f08a8f2294a07
Aoc4456 <sizu44567789@gmail.com>
2021-08-07 12:51:55 +0900

    外部とのインターフェースを作成

--- app/src/main/java/com/aoc4456/radarchart/component/multiinputrowview/InputRowView.kt
+++ app/src/main/java/com/aoc4456/radarchart/component/multiinputrowview/InputRowView.kt
@@ -2,8 +2,11 @@ package com.aoc4456.radarchart.component.multiinputrowview
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import com.aoc4456.radarchart.R
+import com.aoc4456.radarchart.component.Stepper
 
 /**
  * 項目名ラベル、EditText、Stepperを１行にまとめたView
@@ -13,9 +16,40 @@ import com.aoc4456.radarchart.R
 class InputRowView(
     context: Context,
     private val attrs: AttributeSet
-) : LinearLayout(context, attrs) {
+) : LinearLayout(context, attrs), InputRowViewInput {
+
+    lateinit var labelTextView: TextView
+    lateinit var editText: EditText
+    lateinit var stepper: Stepper
 
     init {
         inflate(context, R.layout.input_row_view, this)
+
+        labelTextView = findViewById(R.id.labelText)
+        editText = findViewById(R.id.editText)
+        stepper = findViewById(R.id.stepper)
     }
+
+    override fun setLabel(label: String) {
+        labelTextView.text = label
+    }
+
+    override fun setValue(value: Double) {
+        editText.setText(value.toInt().toString())
+        stepper.value = value
+    }
+
+    override fun setMaximum(maximum: Int) {
+        stepper.maximumValue = maximum.toDouble()
+    }
+}
+
+interface InputRowViewInput {
+    fun setLabel(label: String)
+    fun setValue(value: Double)
+    fun setMaximum(maximum: Int)
+}
+
+interface InputRowViewOutput {
+    fun onChangeValue(view: InputRowView, value: Double)
 }
