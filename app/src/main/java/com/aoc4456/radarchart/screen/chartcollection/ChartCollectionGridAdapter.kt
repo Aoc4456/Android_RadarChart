cbf8ec84c63fb2f3e10696edb6a0918efb3c1f72
Aoc4456 <sizu44567789@gmail.com>
2021-08-13 12:16:57 +0900

    Grid用のAdapter

---
+++ app/src/main/java/com/aoc4456/radarchart/screen/chartcollection/ChartCollectionGridAdapter.kt
@@ -0,0 +1,48 @@
+package com.aoc4456.radarchart.screen.chartcollection
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.aoc4456.radarchart.databinding.ChartCollectionGridItemBinding
+import com.aoc4456.radarchart.datasource.database.MyChartWithValue
+import com.aoc4456.radarchart.util.ChartDataUtil
+
+class ChartCollectionGridAdapter(private val viewModel: ChartCollectionViewModel) :
+    ListAdapter<MyChartWithValue, ChartCollectionGridAdapter.ViewHolder>(ChartCollectionDiffCallBack()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.createViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, viewModel)
+    }
+
+    class ViewHolder private constructor(val binding: ChartCollectionGridItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: MyChartWithValue, viewModel: ChartCollectionViewModel) {
+            val radarData = ChartDataUtil.getRadarDataFromValues(
+                item.myChart.color,
+                item.values.map { it.value.toInt() }
+            )
+            binding.radarChart.let {
+                it.data = radarData
+                it.yAxis.axisMaximum = viewModel.maximum.toFloat()
+                // TODO ラベルをセット
+                it.notifyDataSetChanged()
+            }
+            binding.title.text = item.myChart.title
+        }
+
+        companion object {
+            fun createViewHolder(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ChartCollectionGridItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
