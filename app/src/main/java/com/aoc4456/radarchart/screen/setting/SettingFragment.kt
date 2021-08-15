883b53c161bdc4ea319fa4e23ef5834348f42b90
Aoc4456 <sizu44567789@gmail.com>
2021-08-15 17:30:20 +0900

    レイアウトをバインディングオブジェクトに変換

--- app/src/main/java/com/aoc4456/radarchart/screen/setting/SettingFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/setting/SettingFragment.kt
@@ -5,18 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.aoc4456.radarchart.R
+import com.aoc4456.radarchart.databinding.SettingFragmentBinding
 
 class SettingFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: SettingFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        binding = SettingFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 }
