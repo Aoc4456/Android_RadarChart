e3138834f14b21776cfd8bd749ee7e78b1a6ae61
Aoc4456 <sizu44567789@gmail.com>
2021-08-25 14:34:57 +0900

    Progressbarの追加

--- app/src/main/java/com/aoc4456/radarchart/screen/setting/PrivacyPolicyFragment.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/setting/PrivacyPolicyFragment.kt
@@ -1,9 +1,12 @@
 package com.aoc4456.radarchart.screen.setting
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aoc4456.radarchart.databinding.PrivacyPolicyFragmentBinding
@@ -28,6 +31,19 @@ class PrivacyPolicyFragment : Fragment() {
         binding.toolbarCloseButton.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        binding.webviewPrivacyPolicy.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                binding.progress.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.progress.visibility = View.GONE
+            }
+        }
+
         binding.webviewPrivacyPolicy.loadUrl("https://radarchart-aocm.web.app/")
     }
 }
