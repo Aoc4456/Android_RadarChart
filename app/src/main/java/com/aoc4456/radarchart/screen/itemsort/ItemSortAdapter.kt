e83ce821a667381cb670d2e9635772c5674ed18b
Aoc4456 <sizu44567789@gmail.com>
2021-08-29 10:58:42 +0900

    ドラッグハンドルを追加

--- app/src/main/java/com/aoc4456/radarchart/screen/itemsort/ItemSortAdapter.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/itemsort/ItemSortAdapter.kt
@@ -1,6 +1,8 @@
 package com.aoc4456.radarchart.screen.itemsort
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
@@ -20,9 +22,18 @@ class ItemSortAdapter(
         return ViewHolder(binding)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = viewModel.labelList[position]
-        holder.binding.label.text = item.text
+        holder.binding.let {
+            it.label.text = item.text
+            it.dragHandle.setOnTouchListener { _, event ->
+                if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                    itemTouchHelper.startDrag(holder)
+                }
+                return@setOnTouchListener true
+            }
+        }
     }
 
     override fun getItemCount(): Int = viewModel.labelList.size
