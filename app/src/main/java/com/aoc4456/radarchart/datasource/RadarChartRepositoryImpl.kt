4b43bed2686173aa28973ab9497e9fd256009838
Aoc4456 <sizu44567789@gmail.com>
2021-08-29 11:04:52 +0900

    グループ更新の処理を修正

--- app/src/main/java/com/aoc4456/radarchart/datasource/RadarChartRepositoryImpl.kt
+++ app/src/main/java/com/aoc4456/radarchart/datasource/RadarChartRepositoryImpl.kt
@@ -21,12 +21,6 @@ class RadarChartRepositoryImpl(
         labels: List<String>,
         oldGroup: GroupWithLabelAndCharts?
     ) {
-        val maxRate = radarChartDao.getMaxRate()
-        group.rate = when (maxRate) {
-            null -> 0
-            else -> maxRate + 1
-        }
-
         val chartLabels = mutableListOf<ChartGroupLabel>()
         for (i in labels.indices) {
             chartLabels.add(
@@ -40,6 +34,11 @@ class RadarChartRepositoryImpl(
 
         withContext(ioDispatcher) {
             if (oldGroup == null) {
+                val maxRate = radarChartDao.getMaxRate()
+                group.rate = when (maxRate) {
+                    null -> 0
+                    else -> maxRate + 1
+                }
                 radarChartDao.saveGroupAndLabel(group, chartLabels)
             } else {
                 radarChartDao.updateGroupAndLabel(group, chartLabels, oldGroup)
