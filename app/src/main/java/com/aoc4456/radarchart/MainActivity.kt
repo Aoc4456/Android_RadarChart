ff6b33494d78ba78a2b77d532adf8df0f8ac63f0
Aoc4456 <sizu44567789@gmail.com>
2021-09-04 16:44:07 +0900

    アプリ起動回数をカウント

--- app/src/main/java/com/aoc4456/radarchart/MainActivity.kt
+++ app/src/main/java/com/aoc4456/radarchart/MainActivity.kt
@@ -2,12 +2,27 @@ package com.aoc4456.radarchart
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aoc4456.radarchart.datasource.sharedpreferences.RadarChartPreferences
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var preferences: RadarChartPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // アプリ起動回数5回毎に、In App Review を要求
+        val appLaunchCount = preferences.loadAppLaunchCount()
+        Timber.d("アプリ起動回数 = $appLaunchCount")
+        if (appLaunchCount >= 5) {
+            preferences.resetAppLaunchCount()
+            Timber.d("アプリ起動回数をリセット")
+        }
     }
 }
--- app/src/main/java/com/aoc4456/radarchart/datasource/sharedpreferences/RadarChartPreferences.kt
+++ app/src/main/java/com/aoc4456/radarchart/datasource/sharedpreferences/RadarChartPreferences.kt
@@ -12,6 +12,26 @@ class RadarChartPreferences @Inject constructor(@ApplicationContext context: Con
 
     private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
 
+    /**
+     * アプリ起動回数 (規定回数到達で In App Review 表示)
+     */
+
+    fun incrementAppLaunchCount() {
+        preferences.edit().putInt(KEY_APP_LAUNCH_COUNT, loadAppLaunchCount() + 1).apply()
+    }
+
+    fun resetAppLaunchCount() {
+        preferences.edit().putInt(KEY_APP_LAUNCH_COUNT, 0).apply()
+    }
+
+    fun loadAppLaunchCount(): Int {
+        return preferences.getInt(KEY_APP_LAUNCH_COUNT, 0)
+    }
+
+    /**
+     * チャート一覧 リスト表示 or グリッド表示
+     */
+
     fun saveCollectionType(type: CollectionType) {
         preferences.edit().putString(KEY_COLLECTION_TYPE, type.name).apply()
     }
@@ -22,6 +42,7 @@ class RadarChartPreferences @Inject constructor(@ApplicationContext context: Con
     }
 
     companion object {
+        const val KEY_APP_LAUNCH_COUNT = "AppLaunchCount"
         const val KEY_COLLECTION_TYPE = "CollectionType"
     }
 }
