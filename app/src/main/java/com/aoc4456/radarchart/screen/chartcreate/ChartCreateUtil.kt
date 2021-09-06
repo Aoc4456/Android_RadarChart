215352254174b3c8d646986322e621ea9be8944e
Aoc4456 <sizu44567789@gmail.com>
2021-09-06 14:48:06 +0900

    改行した下段のテキストを中央に配置する

--- app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateUtil.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/chartcreate/ChartCreateUtil.kt
@@ -1,5 +1,7 @@
 package com.aoc4456.radarchart.screen.chartcreate
 
+import android.graphics.Paint
+
 object ChartCreateUtil {
 
     fun getValuedLabelList(labels: List<String>?, values: List<Int>?): List<String> {
@@ -16,6 +18,27 @@ object ChartCreateUtil {
     }
 
     private fun getValuedText(label: String, value: Int): String {
-        return "$label \n $value"
+        val space = getDiffSpace(label, value.toString())
+        return "$label\n$space$value"
+    }
+
+    /**
+     * 下段のテキストを上段の中央にあたる位置に配置するために、必要な分のスペースを返す
+     */
+    private fun getDiffSpace(label: String, value: String): String {
+        val paint = Paint()
+
+        val labelWidth = paint.measureText(label, 0, label.length)
+        val valueWidth = paint.measureText(value, 0, value.length)
+        val diff = labelWidth - valueWidth
+
+        val spaceWidth = paint.measureText(" ", 0, " ".length)
+        val spaceCount = ((diff / spaceWidth) / 2).toInt()
+
+        var string = ""
+        if (spaceCount > 0) {
+            repeat(spaceCount) { string += " " }
+        }
+        return string
     }
 }
