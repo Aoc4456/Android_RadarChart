74cb0a30f99f6bbe6ff479a211da62e14dedfbe4
Aoc4456 <sizu44567789@gmail.com>
2021-09-15 21:07:37 +0900

    英語のリソースを追加 (+1 squashed commit) Squashed commits: [5ae8d0f] 英語のリソースを追加

--- app/src/main/java/com/aoc4456/radarchart/screen/groupcreate/GroupCreateUtil.kt
+++ app/src/main/java/com/aoc4456/radarchart/screen/groupcreate/GroupCreateUtil.kt
@@ -1,6 +1,7 @@
 package com.aoc4456.radarchart.screen.groupcreate
 
 import com.aoc4456.radarchart.datasource.database.ChartGroupLabel
+import java.util.*
 
 object GroupCreateUtil {
     fun getExactlySizedTextList(textList: List<String>, numberOfItems: Int): List<String> {
@@ -12,12 +13,20 @@ object GroupCreateUtil {
     }
 
     fun getMaximumSizeTextList(labelList: List<ChartGroupLabel>): MutableList<String> {
-        val tempList = defaultTextList.toMutableList()
+        val tempList = getDefaultTextList().toMutableList()
         for (i in labelList.indices) {
             tempList[i] = labelList[i].text
         }
         return tempList
     }
 
-    val defaultTextList = listOf("項目1", "項目2", "項目3", "項目4", "項目5", "項目6", "項目7", "項目8")
+    fun getDefaultTextList(): List<String> {
+        val locale = Locale.getDefault()
+        val language = locale.language
+        return if (language == "ja") {
+            listOf("項目1", "項目2", "項目3", "項目4", "項目5", "項目6", "項目7", "項目8")
+        } else {
+            listOf("Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7", "Item8")
+        }
+    }
 }
