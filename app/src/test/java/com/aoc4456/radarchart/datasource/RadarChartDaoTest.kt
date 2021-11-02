f1ba7ed6497e1eacd0dcf388edffbe31ce189214
Aoc4456 <sizu44567789@gmail.com>
2021-11-02 19:12:04 +0900

    Daoのテスト用のファイルを追加

---
+++ app/src/test/java/com/aoc4456/radarchart/datasource/RadarChartDaoTest.kt
@@ -0,0 +1,16 @@
+package com.aoc4456.radarchart.datasource
+
+import androidx.room.Room
+import com.aoc4456.radarchart.datasource.database.RadarChartDatabase
+import org.junit.Before
+import org.junit.runner.RunWith
+
+class RadarChartDaoTest{
+
+    private lateinit var database: RadarChartDatabase
+
+    @Before
+    fun initDb(){
+    }
+
+}
