74a9f26c7b6de7561335628e4f80ee9863bb6ad4
Aoc4456 <sizu44567789@gmail.com>
2021-11-06 19:53:49 +0900

    Daoのテストコードを追加

--- app/src/test/java/com/aoc4456/radarchart/datasource/RadarChartDaoTest.kt
+++ app/src/test/java/com/aoc4456/radarchart/datasource/RadarChartDaoTest.kt
@@ -4,13 +4,10 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.aoc4456.radarchart.datasource.database.ChartGroup
-import com.aoc4456.radarchart.datasource.database.ChartGroupLabel
-import com.aoc4456.radarchart.datasource.database.RadarChartDatabase
+import com.aoc4456.radarchart.datasource.database.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
@@ -41,6 +38,10 @@ class RadarChartDaoTest {
     @After
     fun closeDb() = database.close()
 
+    /**
+     * Create Test
+     */
+
     @Test
     fun insertGroupAndGetById() = runBlocking {
         // GIVEN - Groupをインサート
@@ -62,17 +63,14 @@ class RadarChartDaoTest {
     }
 
     @Test
-    fun insertGroupAndLabelsAndGetById() = runBlocking {
+    fun saveGroupAndLabelsAndGetById() = runBlocking {
         // GIVEN - GroupLabelをインサート
         val group = ChartGroup(title = "test", color = 12345, maximumValue = 200)
         val labels = mutableListOf<ChartGroupLabel>()
         for (i in 0..5) {
             labels.add(ChartGroupLabel(chartGroupId = group.id, index = i, text = "ラベル$i"))
         }
-        database.radarChartDao().insertGroup(group)
-        labels.forEach {
-            database.radarChartDao().insertGroupLabel(it)
-        }
+        database.radarChartDao().saveGroupAndLabel(group, labels)
 
         // WHEN GroupLabelをデータベースから取得
         val loaded = database.radarChartDao().getGroupWithLabelAndCharts(group.id)
@@ -85,9 +83,38 @@ class RadarChartDaoTest {
     }
 
     @Test
-    fun insertAndDeleteGroup() = runBlocking {
+    fun saveGroupAndChart() = runBlocking {
         // GIVEN
-        // GIVEN - Groupをインサート
+        val group = ChartGroup(title = "test", color = 12345, maximumValue = 200)
+        val labels = mutableListOf<ChartGroupLabel>()
+        for (i in 0..5) {
+            labels.add(ChartGroupLabel(chartGroupId = group.id, index = i, text = "ラベル$i"))
+        }
+        database.radarChartDao().saveGroupAndLabel(group, labels)
+
+        val myChart = MyChart(chartGroupId = group.id, title = "chartTitle", color = 12345)
+        val chartValues = mutableListOf<ChartValue>()
+        for (i in 0..5) {
+            chartValues.add(ChartValue(myChartId = myChart.id, index = i, value = 60.0))
+        }
+        database.radarChartDao().saveChartAndValues(myChart, chartValues)
+
+        // WHEN
+        val loaded = database.radarChartDao().getGroupWithLabelAndCharts(group.id)
+
+        // THEN
+        assertThat(loaded.group.id, `is`(group.id))
+        assertThat(loaded.labelList.size, `is`(6))
+        assertThat(loaded.chartList.size, `is`(1))
+        assertThat(loaded.chartList.first().title, `is`(myChart.title))
+    }
+
+    /**
+     * Update Test
+     */
+    @Test
+    fun updateGroup() = runBlocking {
+        // GIVEN
         val group = ChartGroup(
             title = "test",
             color = 12345,
@@ -95,7 +122,57 @@ class RadarChartDaoTest {
         )
         database.radarChartDao().insertGroup(group)
 
+        val newGroup = ChartGroup(
+            id = group.id,
+            title = "update",
+            color = 67890,
+            maximumValue = 300
+        )
+        database.radarChartDao().updateGroup(newGroup)
+
         // WHEN
+        val loaded = database.radarChartDao().getGroupById(group.id)
+
+        // THEN
+        assertThat(loaded, notNullValue())
+        assertThat(loaded.id, `is`(group.id))
+        assertThat(loaded.id, `is`(newGroup.id))
+        assertThat(loaded.color, `is`(newGroup.color))
+        assertThat(loaded.title, `is`("update"))
+        assertThat(loaded.maximumValue, `is`(300))
+    }
+
+    /**
+     * Delete Test
+     */
+    @Test
+    fun deleteGroup() = runBlocking {
+        // GIVEN
+        val group = ChartGroup(title = "test", color = 12345, maximumValue = 200)
+        val labels = mutableListOf<ChartGroupLabel>()
+        for (i in 0..5) {
+            labels.add(ChartGroupLabel(chartGroupId = group.id, index = i, text = "ラベル$i"))
+        }
+        database.radarChartDao().saveGroupAndLabel(group, labels)
+
+        val myChart = MyChart(chartGroupId = group.id, title = "chartTitle", color = 12345)
+        val chartValues = mutableListOf<ChartValue>()
+        for (i in 0..5) {
+            chartValues.add(ChartValue(myChartId = myChart.id, index = i, value = 60.0))
+        }
+        database.radarChartDao().saveChartAndValues(myChart, chartValues)
         database.radarChartDao().deleteGroup(group.id)
+
+        // WHEN
+        val loaded = database.radarChartDao().getGroupListWithDetail()
+        val loadedGroup = database.radarChartDao().getGroupById(group.id)
+        val loadedGroupLabel = database.radarChartDao().getGroupLabel(group.id)
+        val loadedCharts = database.radarChartDao().getChartList(group.id)
+
+        // THEN
+        assertThat(loaded.isEmpty(), `is`(true))
+        assertThat(loadedGroup, `is`(nullValue()))
+        assertThat(loadedGroupLabel.isEmpty(), `is`(true))
+        assertThat(loadedCharts.isEmpty(), `is`(true))
     }
 }
