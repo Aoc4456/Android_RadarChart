b6e47d7f563aad9c2ff34302b17c430388a53129
Aoc4456 <sizu44567789@gmail.com>
2021-11-08 13:02:00 +0900

    ViewModelのテスト用にFakeRepositoryを作成

--- app/src/test/java/com/aoc4456/radarchart/datasource/fake/FakeRepository.kt
+++ app/src/test/java/com/aoc4456/radarchart/datasource/fake/FakeRepository.kt
@@ -1,34 +1,79 @@
 package com.aoc4456.radarchart.datasource.fake
 
 import com.aoc4456.radarchart.datasource.RadarChartRepository
-import com.aoc4456.radarchart.datasource.database.*
+import com.aoc4456.radarchart.datasource.database.ChartGroup
+import com.aoc4456.radarchart.datasource.database.ChartGroupLabel
+import com.aoc4456.radarchart.datasource.database.ChartValue
+import com.aoc4456.radarchart.datasource.database.GroupWithLabelAndCharts
+import com.aoc4456.radarchart.datasource.database.MyChart
+import com.aoc4456.radarchart.datasource.database.MyChartWithValue
+import com.aoc4456.radarchart.datasource.database.OrderBy
 import com.aoc4456.radarchart.screen.chartcollection.CollectionType
+import com.aoc4456.radarchart.util.MyChartOrder
 
 /**
  * ViewModelのテストのためのFakeリポジトリ
+ * // TODO コンストラクタでデータを受け取って、initで保存するよう変更する
  */
 class FakeRepository : RadarChartRepository {
 
     private val fakeDao = FakeDao()
+
+    /**
+     * Create
+     */
 
     override suspend fun saveGroup(
         group: ChartGroup,
         labels: List<String>,
         oldGroup: GroupWithLabelAndCharts?
     ) {
-        TODO("Not yet implemented")
+        val chartLabels = mutableListOf<ChartGroupLabel>()
+        for (i in labels.indices) {
+            chartLabels.add(
+                ChartGroupLabel(
+                    chartGroupId = group.id,
+                    index = i,
+                    text = labels[i]
+                )
+            )
+        }
+        if (oldGroup == null) {
+            val maxRate = fakeDao.getMaxRate()
+            group.rate = when (maxRate) {
+                null -> 0
+                else -> maxRate + 1
+            }
+            fakeDao.saveGroupAndLabel(group, chartLabels)
+        } else {
+            fakeDao.updateGroupAndLabel(group, chartLabels, oldGroup)
+        }
     }
 
     override suspend fun saveChart(chart: MyChart, values: List<Int>) {
-        TODO("Not yet implemented")
+        val chartValueList = mutableListOf<ChartValue>()
+        for (i in values.indices) {
+            chartValueList.add(
+                ChartValue(
+                    myChartId = chart.id,
+                    index = i,
+                    value = values[i].toDouble()
+                )
+            )
+        }
+        fakeDao.saveChartAndValues(chart, chartValueList)
     }
 
+    /**
+     * Read
+     */
+
     override suspend fun getGroupList(): List<GroupWithLabelAndCharts> {
-        TODO("Not yet implemented")
+        return fakeDao.getGroupListWithDetail()
     }
 
     override suspend fun getGroupById(groupId: String): GroupWithLabelAndCharts {
-        TODO("Not yet implemented")
+        return fakeDao.getGroupWithLabelAndCharts(groupId)
     }
 
     override suspend fun getSortedChartList(
@@ -36,41 +81,65 @@ class FakeRepository : RadarChartRepository {
         sortIndex: Int,
         orderBy: OrderBy
     ): List<MyChartWithValue> {
-        TODO("Not yet implemented")
+        val list = mutableListOf<MyChartWithValue>()
+        val chartList = fakeDao.getChartList(groupId)
+        chartList.forEach {
+            list.add(
+                MyChartWithValue(
+                    myChart = it,
+                    values = fakeDao.getChartValueById(it.id)
+                )
+            )
+        }
+        return MyChartOrder.getSortedChartList(list, sortIndex, orderBy)
     }
 
+    /**
+     * Update
+     */
+
     override suspend fun changeAscDesc(groupId: String, orderBy: OrderBy) {
-        TODO("Not yet implemented")
+        fakeDao.changeAscDesc(groupId, orderBy)
     }
 
     override suspend fun updateSortIndex(groupId: String, sortIndex: Int) {
-        TODO("Not yet implemented")
+        fakeDao.updateSortIndex(groupId, sortIndex)
     }
 
     override suspend fun setGroupRates(list: List<ChartGroup>) {
-        TODO("Not yet implemented")
+        fakeDao.setRates(list)
     }
 
     override suspend fun swapGroupLabel(
         group: GroupWithLabelAndCharts,
         newList: List<ChartGroupLabel>
     ) {
-        TODO("Not yet implemented")
+        fakeDao.swapGroupLabel(group, newList)
     }
 
+    /**
+     * Delete
+     */
+
     override suspend fun deleteGroup(groupId: String) {
-        TODO("Not yet implemented")
+        fakeDao.deleteGroup(groupId)
     }
 
     override suspend fun deleteMyChart(chartId: String) {
-        TODO("Not yet implemented")
+        fakeDao.deleteMyChart(chartId)
     }
 
+    /**
+     * SharedPreferences
+     */
+
+    private var saveType = CollectionType.LIST
+
     override fun saveCollectionType(type: CollectionType) {
-        TODO("Not yet implemented")
+        saveType = type
     }
 
     override fun loadCollectionType(): CollectionType {
-        TODO("Not yet implemented")
+        return saveType
     }
 }
