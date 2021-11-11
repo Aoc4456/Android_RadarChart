43c0bd6a15c618ae7de76e4d199913438e4f61d8
Aoc4456 <sizu44567789@gmail.com>
2021-11-11 21:26:31 +0900

    SettingFragmentのテストを１件追加(失敗)

--- app/src/androidTest/java/com/aoc4456/radarchart/di/TestRepositoryModule.kt
+++ app/src/androidTest/java/com/aoc4456/radarchart/di/TestRepositoryModule.kt
@@ -1,1 +1,24 @@
 package com.aoc4456.radarchart.di
+
+import com.aoc4456.radarchart.datasource.RadarChartRepository
+import com.aoc4456.radarchart.datasource.RadarChartRepositoryImpl
+import com.aoc4456.radarchart.datasource.fake.FakeRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+/**
+ * Hilt will inject a [FakeRepository] instead of a [RadarChartRepositoryImpl].
+ */
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [RadarChartRepositoryModule::class]
+)
+abstract class TestRepositoryModule {
+    @Singleton
+    @Binds
+    abstract fun bindRepository(repository: FakeRepository): RadarChartRepository
+}
--- app/src/androidTest/java/com/aoc4456/radarchart/screen/setting/SettingFragmentTest.kt
+++ app/src/androidTest/java/com/aoc4456/radarchart/screen/setting/SettingFragmentTest.kt
@@ -1,9 +1,17 @@
 package com.aoc4456.radarchart.screen.setting
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.aoc4456.radarchart.R
+import com.aoc4456.radarchart.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.hamcrest.core.IsNot.not
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,6 +24,17 @@ class SettingFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
     @Test
-    fun displayItem() {}
+    fun displayItem() {
+        // WHEN
+        launchFragmentInHiltContainer<SettingFragment>()
+
+        // THEN
+        onView(withId(R.id.privacy_policy)).check(matches(isDisplayed()))
+    }
 }
