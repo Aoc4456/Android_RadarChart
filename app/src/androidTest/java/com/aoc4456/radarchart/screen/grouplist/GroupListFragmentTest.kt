9985ffbef8548bb0b7b7fff12088c296175c422e
Aoc4456 <sizu44567789@gmail.com>
2021-11-14 14:16:33 +0900

    GroupListFragmentのテストを追加

--- app/src/androidTest/java/com/aoc4456/radarchart/screen/grouplist/GroupListFragmentTest.kt
+++ app/src/androidTest/java/com/aoc4456/radarchart/screen/grouplist/GroupListFragmentTest.kt
@@ -3,17 +3,20 @@ package com.aoc4456.radarchart.screen.grouplist
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.aoc4456.radarchart.MainActivity
+import com.aoc4456.radarchart.R
 import com.aoc4456.radarchart.datasource.RadarChartRepository
 import com.aoc4456.radarchart.datasource.database.ChartGroup
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -51,8 +54,9 @@ class GroupListFragmentTest {
         launchActivity()
 
         // THEN
-        onView(ViewMatchers.withText("TITLE1"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.floatingActionButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.toolbar_sort_button)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.btnSetting)).check(matches(isDisplayed()))
     }
 
     private fun launchActivity(): ActivityScenario<MainActivity>? {
